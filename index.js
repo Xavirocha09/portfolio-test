2223ed613e8f74fa0b77a1c479e20f0657c4c9ea
javi <rocha.javier.9999@gmail.com>
2024-07-24 18:28:34 +0200

    changes to js to test if it works

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
 const submitBtn = document.getElementById('submitButton')
 
     submitBtn.addEventListener('click', function() {
-    var name = document.getElementById('input-name').value;
-    var lastName = document.getElementById('input-lastname').value;
-    var email = document.getElementById('input-email').value;
-    var subject = document.getElementById('input-subject').value;
-    var message = document.getElementById('input-message').value;
-
-    console.log('Button clicked');
-    console.log('Name:', name);
-    console.log('Last Name:', lastName);
-    console.log('Email:', email);
-    console.log('Subject:', subject);
-    console.log('Message:', message);
+    let name = document.getElementById('input-name').value;
+    let lastName = document.getElementById('input-lastname').value;
+    let email = document.getElementById('input-email').value;
+    let subject = document.getElementById('input-subject').value;
+    let message = document.getElementById('input-message').value;
 
     if (!name || !lastName || !subject || !message) {
-        return alert('Please fill out all required fields (Name, Last Name, Subject, Message).')
-        
+        submitBtn.outerHTML = 'Please fill out all required fields (Name, Last Name, Subject, Message).'
+        document.getElementById('input-name').focus();
+    }
+    else{
+        submitBtn.outerHTML = '<p id="submitMessage" class="submit-message" aria-live="polite">Message sent! ✅</p>';
+
     }
 
-    var mailtoLink = `mailto:rocha.javier.9999@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent('Name: ' + name + '\nLast Name: ' + lastName + '\nEmail: ' + email + '\n\n' + message)}`;
-    console.log('Mailto Link:', mailtoLink);
+    const mailtoLink = `mailto:rocha.javier.9999@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent('Name: ' + name + '\nLast Name: ' + lastName + '\nEmail: ' + email + '\n\n' + message)}`;
     window.location.href = mailtoLink;
 });
-
-function sendMessage() {    
-    // Replace the button with a paragraph
-    submitBtn.outerHTML = '<p id="submitMessage" class="submit-message" aria-live="polite">Message sent! ✅</p>';
-    
-    // Get the home link element and add focus to it
-    document.getElementById('homeLink').focus();
-}
