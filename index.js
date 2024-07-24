efdcf5394a9624a5db12f8c15252ce9b5caecba8
javi <rocha.javier.9999@gmail.com>
2024-07-24 16:08:50 +0200

    test java button "sent message"

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ document.getElementById('send-button').addEventListener('click', function() {
     console.log('Mailto Link:', mailtoLink);
     window.location.href = mailtoLink;
 });
+
+function sendMessage() {
+    // Get the button element
+    const button = document.getElementById('submitButton');
+    
+    // Replace the button with a paragraph
+    button.outerHTML = '<p id="submitMessage" class="submit-message" aria-live="polite">Message sent! ✅</p>';
+    
+    // Get the home link element and add focus to it
+    document.getElementById('homeLink').focus();
+}
