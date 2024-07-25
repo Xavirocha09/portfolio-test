5a0b0f08d6419b3dbad3f3ddc3f68eda5f93fb9e
javi <rocha.javier.9999@gmail.com>
2024-07-25 13:09:09 +0200

    Top button and structure changes

--- index.js
+++ index.js
@@ -65,3 +65,21 @@ function sendMessage() {
     }
     
 }
+let topBtn = document.getElementById("top")
+
+window.onscroll = function() {scroll()};
+
+function scroll(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+      } else {
+        topBtn.style.display = "none";
+      }
+    }
+    
+
+    // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
