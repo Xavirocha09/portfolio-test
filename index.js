3384550095e12b9d8352158f8c56032c5194b028
javi <rocha.javier.9999@gmail.com>
2024-07-25 02:03:44 +0200

    successfully send email

--- index.js
+++ index.js
@@ -31,31 +31,36 @@ function sendMessage() {
 
     if (name && lastName && email) {
         // Assuming subject is either a field in your form or a predefined variable
-        const subject = "Contact Form Submission"; // Set a default subject if not capturing from form
-
+        const submitBtn= document.getElementById("submitButton")
+        const subject = document.getElementById("input-subject")
         const mailtoLink = `mailto:rocha.javier.9999@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent('Name: ' + name + '\nLast Name: ' + lastName + '\nEmail: ' + email + '\n\n' + message)}`;
         
         window.open(mailtoLink, '_blank');
-
-        submitMsg.innerHTML = '<p id="submitMessage" class="submit-message" aria-live="polite">Message sent! ✅</p>';
+        submitMsg.innerHTML ='<p id="submitMessage" class="submit-message" aria-live="polite">Success! ✅</p>'
+        submitBtn.outerHTML = '<br> <a href="index.html"><button>Home</button></a><br> <a href="projects.html"><button>check out my Projects Page</button></a><br><a href="contact.html"><button>Send another message</button></a>'
         document.getElementById('homeLink').focus(); // Assuming 'homeLink' exists
     } else {
-        // Determine which field is missing and set focus accordingly
+        submitMsg.innerHTML = " "// Determine which field is missing and set focus accordingly
         if (!name) {
             document.getElementById('input-name').focus();
-        
-             submitMsg.innerHTML += '<p class="error-message" aria-live="assertive">Please enter your Name </p>';
+             submitMsg.innerHTML += '<p class="error-message" aria-live="assertive">Please enter your Name</p>';
         } else if (!lastName) {
+           
             document.getElementById('input-lastname').focus();
             submitMsg.innerHTML += '<p class="error-message" aria-live="assertive">Please enter your Last Name </p>'
+            
         } else if (!email) {
+            
             document.getElementById('input-email').focus();
             submitMsg.innerHTML += '<p class="error-message" aria-live="assertive">Please enter your Email </p>'
-        } else if (!message) {
-            document.getElementById('input-message').focus();
-            submitMsg.innerHTML += '<p class="error-message" aria-live="assertive">Please enter your Message </p>'
+
+        } else if (!subject) {
+            
+            document.getElementById('input-subject').focus();
+            submitMsg.innerHTML += '<p class="error-message" aria-live="assertive">Please enter a subject </p>'
         }
 
        
     }
+    
 }
