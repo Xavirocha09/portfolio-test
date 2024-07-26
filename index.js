9550064d5c7d037872fdcb5a5b0778ec783a1141
javi <rocha.javier.9999@gmail.com>
2024-07-26 20:30:51 +0200

    copy code

--- index.js
+++ index.js
@@ -103,3 +103,11 @@ function topFunction() {
         linkedInBtn.innerHTML = originalLinkedInHTML;
         phoneBtn.innerHTML = originalPhoneHTML;
     }
+
+    function copyToClipboard(text) {
+        navigator.clipboard.writeText(text).then(() => {
+            alert('Copied to clipboard: ' + text);
+        }).catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+    }
